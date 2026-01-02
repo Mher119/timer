867b2a5221e16eff608f92429e46d42219740c0c
Mher119 <korkotyanmher1994@gmail.com>
2026-01-02 23:13:23 +0400

    3th commit

--- app/components/TimerDisplay.tsx
+++ app/components/TimerDisplay.tsx
@@ -24,7 +24,7 @@ export default function TimerDisplay({ seconds, mode }: Props) {
         <span
           className="font-bold text-black text-center"
           style={{
-            fontSize: "clamp(5rem, 80vw, 70vh)", // bigger size now
+            fontSize: "clamp(5rem, 80vw, 100vh)", // bigger size now
             lineHeight: 1,
             wordBreak: "break-word",
             maxWidth: "100%",       // prevents overflow on mobile
